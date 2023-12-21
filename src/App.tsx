d597b3b9149a51359475e3d089b2fb238e112147
Oscar-FV <oscar.flores.vqz@gmail.com>
2023-12-21 15:05:19 -0600

    starting movile

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import Logo from './components/Logo/Logo';
 import Container from './components/Container/Container';
 import Tittle from './components/Tittle/Tittle';
 import AboutMe from './components/AboutMe/AboutMe';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <>
-      <Container className="container font-newake flex justify-center items-center">
+      <NavBar />
+      <Container className="container font-newake flex flex-col justify-center items-center">
         <Logo />
       </Container>  
 
--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,19 +1,18 @@
-import miPortrait from "../../assets/photos/me.webp";
+import miPortrait from "../../assets/imgs/me.webp";
 import { downloadCV } from "./AboutMe.controller";
 
 const AboutMe = () => {
   return (
-    <div className="h-fit lg:h-full flex flex-col-reverse lg:flex-row justify-center items-center">
-      
+    <div className="mt-5 mx-8 h-fit lg:h-full flex flex-col-reverse lg:flex-row justify-center items-center lg:items-start">
       <img
         className="lg:pl-8 w-10/12 lg:w-2/5 h-10/12 lg:h-full object-cover"
         src={miPortrait}
         alt="A Very Talented web Dev"
       />
 
-<div className="w-10/12 lg:w-full lg:sticky lg:top-0">
-        <p className="text-white text-xl lg:text-2xl font-worksans lg:h-fit whitespace-pre-wrap tracking-[2px] flex-1 lg:px-6">
-          Hi! <br /> 
+      <div className="w-10/12 lg:w-full lg:sticky lg:top-0 ">
+        <p className="text-white text-xl lg:text-2xl font-worksans lg:h-fit whitespace-pre-wrap tracking-[2px] flex-1 lg:px-10">
+          Hi! <br />
           My name is Oscar Flores.
           <br />I am a{" "}
           <span className="text-accent font-semibold">
@@ -29,8 +28,11 @@ const AboutMe = () => {
           streamline outdated processes.
         </p>
 
-        <div className="flex justify-end lg:px-14">
-          <button className="btn btn-primary  my-5 lg:my-10" onClick={downloadCV}>
+        <div className="flex justify-end lg:px-32">
+          <button
+            className="btn btn-primary  my-5 lg:my-10"
+            onClick={downloadCV}
+          >
             Get my CV
           </button>
         </div>
