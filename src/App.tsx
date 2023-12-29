f071ef15bdc1763d7c0c4fd5f75953f777813c4c
Oscar-FV <oscar.flores.vqz@gmail.com>
2023-12-29 15:36:33 -0600

    fixed responsive (again)

--- src/App.tsx
+++ src/App.tsx
@@ -17,22 +17,21 @@ function App() {
         <AboutMe />
       </Container>
 
-      <Container className="mx-20 lg:mx-16">
+      <Container className="mx-10 lg:mx-16">
         <Tittle tittle="Stack" />
-        <div className="py-5">
           <div className="grid grid-cols-4 gap-4 lg:gap-10">
-            <p className="col-span-4 lg:col-span-2 text-white text-xl lg:text-2xl font-worksans lg:h-fit whitespace-pre-wrap tracking-[2px] mb-8 lg:px-10 xl:pr-5">
+            <p className="col-span-4 lg:col-span-2 text-white text-base lg:text-2xl font-worksans lg:h-fit whitespace-pre-wrap tracking-[2px] mb-8 lg:px-10 xl:pr-5">
               I enjoy crafting minimalist and elegant solutions that help
               businesses enhance their outdated processes, bringing a
               technological twist to their operations.
             </p>
-            <div className="col-span-2 lg:col-span-1 py-5 gap-5 flex flex-col items-center text-primary text-8xl">
-              <div className="flex gap-20">
+            <div className="col-span-2 lg:col-span-1 py-5 gap-5 flex flex-col items-center text-primary text-6xl lg:text-8xl">
+              <div className="flex gap-5">
                 <Icon icon="uil:react" />
               </div>
             </div>
-            <div className="col-span-2 lg:col-span-1 py-5 gap-5 flex flex-col items-center text-primary text-8xl">
-              <div className="flex gap-20">
+            <div className="col-span-2 lg:col-span-1 py-5 gap-5 flex flex-col items-center text-primary text-6xl lg:text-8xl">
+              <div className="flex gap-5">
                 <Icon icon="la:node" />
                 <Icon icon="simple-icons:nestjs" />
               </div>
@@ -42,7 +41,6 @@ function App() {
               </div>
             </div>
           </div>
-        </div>
       </Container>
     </>
   );
