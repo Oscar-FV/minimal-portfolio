387a86f12a765178df4af67b54084d179cdf4714
Oscar-FV <oscar.flores.vqz@gmail.com>
2024-01-03 19:42:33 -0600

    ended contact module

---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,63 @@
+import { Icon } from "@iconify/react/dist/iconify.js";
+import ContactForm from "./ContactForm";
+import { DateTime } from 'luxon';
+import { useEffect, useState } from "react";
+
+const Contact = () => {
+
+    const [timeCity, setTimeCity] = useState(DateTime.now().setZone('America/Mexico_City'));
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTimeCity(DateTime.now().setZone('America/Mexico_City'));
+        }, 1000);
+    
+        return () => clearInterval(intervalId);
+      }, []);
+
+
+  return (
+    <div className="flex flex-col lg:flex-row mx-14 lg:mx-0 lg:gap-x-16 gap-y-10 lg:px-0 lg:w-full">
+      <div className="flex flex-col justify-center lg:w-[70%] gap-y-2 lg:gap-y-4">
+        <h1 className="font-worksans text-4xl lg:text-6xl font-semibold text-white">
+          Want to work togheter?
+        </h1>
+        <p className="font-worksans text-lg lg:text-3xl text-white">
+          Leave me a message and let's get in touch!
+        </p>
+        <ContactForm/>
+      </div>
+
+      <div className="flex flex-col gap-y-16 grow text-white font-worksans tracking-[2px]">
+        
+        <div>
+            <h3 className="text-3xl text-accent font-semibold mb-1"> Contact Details </h3>
+            <a href="mailto:oscar.flores.vqz@gmai.com" className="text-lg text-white font-normal contact-list"> oscar.flores.vqz@gmai.com </a>
+        </div>
+
+        <div>
+            <h3 className="text-3xl text-accent font-semibold mb-2"> Where To Find Me </h3>
+            
+            <div className="flex items-center gap-x-2 mb-3">
+                <Icon icon="mdi:linkedin" className="text-white text-3xl"/>
+                <a href="https://www.linkedin.com/in/oscar-flores-vázquez-345647243/" className="text-lg text-white font-normal contact-list"> LinkedIn </a>
+            </div>
+            <div className="flex items-center gap-x-2 mb-3">
+                <Icon icon="mdi:github"  className="text-white text-3xl"/>
+                <a href="https://github.com/Oscar-FV" className="text-lg text-white font-normal contact-list"> GitHub </a>
+            </div>
+        </div>
+
+        <div>
+            <h3 className="text-3xl text-accent font-semibold mb-1"> Location </h3>
+            <p className="text-2xl text-white font-normal"> San Luis Potosí, México </p>
+            <p className="text-2xl text-white font-normal"> {timeCity.toFormat('HH:mm:ss')} </p>
+        </div>
+
+      </div>
+    </div>
+    
+  );
+};
+
+export default Contact;
---
+++ src/components/Contact/ContactForm.tsx
@@ -0,0 +1,63 @@
+const ContactForm = () => {
+  return (
+    <>
+      <form className="text-5xl font-worksans mt-8 lg:mt-16">
+        <div className="grid md:grid-cols-2 md:gap-6">
+          <div className="relative z-0 w-full mb-5 group">
+            <input
+              type="text"
+              name="floating_first_name"
+              id="floating_first_name"
+              className="block py-2.5 px-0 w-full text-lg text-white bg-transparent border-0 border-b-2 border-white appearance-none focus:outline-none focus:ring-0 focus:border-accent peer"
+              placeholder=" "
+              required
+            />
+            <label
+              for="floating_first_name"
+              className="peer-focus:font-medium absolute text-lg text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-accent peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Your Name
+            </label>
+          </div>
+          <div className="relative z-0 w-full mb-5 group">
+            <input
+              type="text"
+              name="floating_first_name"
+              id="floating_first_name"
+              className="block py-2.5 px-0 w-full text-lg text-white bg-transparent border-0 border-b-2 border-white appearance-none focus:outline-none focus:ring-0 focus:border-accent peer"
+              placeholder=" "
+              required
+            />
+            <label
+              for="floating_first_name"
+              className="peer-focus:font-medium absolute text-lg text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-accent peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Your Email
+            </label>
+          </div>
+        </div>
+
+        <div className="relative z-0 w-full mt-5 group">
+        <textarea
+              name="floating_first_name"
+              id="floating_first_name"
+              className="block py-2.5 px-0 w-full text-lg text-white bg-transparent border-0 border-b-2 border-white appearance-none focus:outline-none focus:ring-0 focus:border-accent peer"
+              rows={4}
+              required
+            />
+            <label
+              for="floating_first_name"
+              className="peer-focus:font-medium absolute text-lg text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-accent peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Your Message
+            </label>
+        </div>
+        <button className="btn btn-primary my-5 lg:my-10 text-base lg:text-lg ">
+          Send Message
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default ContactForm;
