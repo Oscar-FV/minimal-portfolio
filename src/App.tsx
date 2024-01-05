9df3e2d7bf7c8e7c89f4731d021a28e7619fe6d5
Oscar-FV <oscar.flores.vqz@gmail.com>
2024-01-05 17:59:18 -0600

    bg color change on scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+import gsap from "gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
+import Scrollbar from "smooth-scrollbar";
+
 import Logo from "./components/Logo/Logo";
 import Container from "./components/Container/Container";
 import Tittle from "./components/Tittle/Tittle";
@@ -6,34 +10,94 @@ import NavBar from "./components/NavBar/NavBar";
 import Stack from "./components/Stack/Stack";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
+import { useEffect } from "react";
 
 function App() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  useEffect(() => {
+    const mainContainer = document.querySelector(".main");
+
+    if (mainContainer) {
+      const scrollBar = Scrollbar.init(mainContainer as HTMLElement, {
+        damping: 0.06,
+        delegateTo: document,
+        alwaysShowTracks: false,
+      });
+
+      ScrollTrigger.defaults({
+        scroller: ".main",
+      });
+
+      ScrollTrigger.scrollerProxy(".main", {
+        scrollTop(value) {
+          if (arguments.length && value !== undefined) {
+            scrollBar.scrollTop = value;
+          }
+          return scrollBar.scrollTop;
+        },
+      });
+
+      scrollBar.addListener(ScrollTrigger.update);
+
+      const sectionColors = document.querySelectorAll("[data-bgcolor]");
+
+      if (sectionColors && sectionColors.length > 0) {
+        (sectionColors).forEach((sectionColor, i) => {
+          const currentBgColor = (sectionColor as HTMLElement).dataset.bgcolor;
+          const prevBgColor = i === 0 ? "" : (sectionColors[i - 1] as HTMLElement).dataset.bgcolor;
+
+          ScrollTrigger.create({
+            trigger: sectionColor,
+            scroller: ".main",
+            start: "top 50%",
+            onEnter: () => animateBackgroundColor(currentBgColor),
+            onLeaveBack: () => animateBackgroundColor(prevBgColor),
+          });
+        });
+      }
+    }
+  }, []);
+
+  const animateBackgroundColor = (sectionColor: string | undefined) => {
+    console.log(sectionColor)
+    gsap.to(".main", {
+      backgroundColor: sectionColor,
+      overwrite: "auto",
+    });
+  };
+
   return (
     <>
       <NavBar />
-      <Container className="font-newake flex flex-col justify-center items-center min-h-screen">
-        <Logo />
-      </Container>
+      <div className="main h-screen overflow-hidden"> 
+        <Container
+          className="font-newake flex flex-col justify-center items-center min-h-screen"
+          bgColor="#09090D"
+        >
+          <Logo />
+        </Container>
 
-      <Container className="xl:mx-32 lg:mx-10">
-        <Tittle tittle="About Me" />
-        <AboutMe />
-      </Container>
+        <Container className="xl:mx-32 lg:mx-10" bgColor="#ADA9D0">
+          <Tittle tittle="About Me" />
+          <AboutMe />
+        </Container>
 
-      <Container className="lg:mt-20 xl:mx-32 lg:mx-10">
-        <Tittle tittle="Stack" />
-        <Stack />
-      </Container>
+        <Container className="lg:mt-20 xl:mx-32 lg:mx-10" bgColor="#09090D">
+          <Tittle tittle="Stack" />
+          <Stack />
+        </Container>
 
-      <Container className="lg:mt-20 xl:mx-32 lg:mx-10">
-        <Tittle tittle="Projects" />
-        <Projects />
-      </Container>
+        <Container className="lg:mt-20 xl:mx-32 lg:mx-10" bgColor="#ADA9D0">
+          <Tittle tittle="Projects" />
+          <Projects />
+        </Container>
 
-      <Container  className="lg:mt-16 xl:mx-32 lg:mx-10">
-        <Tittle tittle="Contact" />
-        <Contact />
-      </Container>
+        <Container className="lg:mt-16 xl:mx-32 lg:mx-10" bgColor="#09090D">
+          <Tittle tittle="Contact" />
+          <Contact />
+        </Container>
+      </div>
     </>
   );
 }
--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -3,12 +3,12 @@ import { ReactNode } from "react";
 interface ContainerProps{
     children: ReactNode;
     className?: string;
-
+    bgColor: string;
 }
 
-const Container = ( {children, className}: ContainerProps ) => {
+const Container = ( {children, className, bgColor}: ContainerProps ) => {
   return (
-    <div className={` py-5 ${className}`} >
+    <div className={` py-5 ${className}`} data-bgcolor={bgColor}>
       
         {children}
 
